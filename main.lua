12e245d2bbbeb9e1cd2570738376508b2aac64c8
APTECH <aryaputra.pro25@gmail.com>
2026-03-04 01:38:58 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
-local gui = Instance.new("ScreenGui")
-gui.Parent = game:GetService("CoreGui")
-
-local img = Instance.new("ImageLabel")
-img.Parent = gui
-img.BackgroundTransparency = 1
-img.Size = UDim2.new(0.3, 0, 0.3, 0)
-img.Position = UDim2.new(0.35, 0, 0.35, 0)
-img.Image = "rbxassetid://111580113698335"
+pcall(function()
+    local gui = Instance.new("ScreenGui")
+    gui.Parent = game:GetService("CoreGui")
+    gui.ResetOnSpawn = false
+    
+    local img = Instance.new("ImageLabel")
+    img.Parent = gui
+    img.Image = "rbxassetid://111580113698335"
+    img.BackgroundTransparency = 1
+    img.Size = UDim2.new(1, 0, 1, 0)
+    img.Position = UDim2.new(0, 0, 0, 0)
+    img.ZIndex = 999
+end)
